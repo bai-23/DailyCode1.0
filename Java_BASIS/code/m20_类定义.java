7384c400d0a7efa52f7fcfba2a3672b62ca88fc1
song <2421662954@qq.com>
2020-07-24 12:06:44 +0800

    1.4

---
+++ Java_BASIS/code/m20_类定义.java
@@ -0,0 +1,16 @@
+public class m20_类定义 {
+    //  定义一个学生类
+    //  属性
+    int stu_id;
+    String name;
+    int age;
+    boolean sex;
+    String addr;
+
+
+
+    public static void main(String[] args){
+        System.out.println("main-begin");
+    }
+}
+
--- Java_BASIS/code/m20_面向对象简介.java
+++
@@ -1,5 +0,0 @@
-public class m20_面向对象简介 {
-    public static void main(String[] args){
-        System.out.println("main-begin");
-    }
-}
