bf1874967aeb994475a616be452951d20cf03bd1
song <2421662954@qq.com>
2020-08-04 09:43:35 +0800

    1

---
+++ shell_BASIS/shell2.0/s1-helloworld.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Name:first
+# Desc:num
+# Path:/shell/s1-helloworld.sh
+# Usage:/
+# Update:2020-07-29
+echo "hello world!"
+
+
+
---
+++ shell_BASIS/shell2.0/s2-pro.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+
+# touch /dir{1..3}
+echo "$(date +'%F %T')创建目录成功!"
+
+# rm -rf/dir*
+echo "$(date +'%F %T')删除目录成功!"
